042d696031d4cc181f0eb277e60e774530964bb3
Omar Rizwan <omar.rizwan@gmail.com>
2019-09-12 16:48:45 -0700

    Works in discrete.

--- trackpad-play.cc
+++ trackpad-play.cc
@@ -29,33 +29,46 @@ extern "C" {
     void MTDeviceStart(MTDeviceRef, int); // thanks comex
 }
 
+#define DISPLAY_WIDTH 8
+#define DISPLAY_HEIGHT 8
+
+UDPFlaschenTaschen *canvas;
+
 int callback(int device, Finger *data, int nFingers, double timestamp, int frame) {
-  for (int i=0; i<nFingers; i++) {
-    Finger *f = &data[i];
+    canvas->Clear();
+    for (int i=0; i<nFingers; i++) {
+        Finger *f = &data[i];
     
-    printf("Frame %7d: Angle %6.2f, ellipse %6.3f x%6.3f; "
-    	   "position (%6.3f,%6.3f) vel (%6.3f,%6.3f) "
-    	   "ID %d, state %d [%d %d?] size %6.3f, %6.3f?\n",
-           f->frame,
-           f->angle * 90 / atan2(1,0),
-           f->majorAxis,
-           f->minorAxis,
-           f->normalized.pos.x,
-           f->normalized.pos.y,
-           f->normalized.vel.x,
-           f->normalized.vel.y,
-           f->identifier, f->state, f->foo3, f->foo4,
-           f->size, f->unk2);
-  }
-  printf("\n");
-  return 0;
+        printf("Frame %7d: Angle %6.2f, ellipse %6.3f x%6.3f; "
+               "position (%6.3f,%6.3f) vel (%6.3f,%6.3f) "
+               "ID %d, state %d [%d %d?] size %6.3f, %6.3f?\n",
+               f->frame,
+               f->angle * 90 / atan2(1,0),
+               f->majorAxis,
+               f->minorAxis,
+               f->normalized.pos.x,
+               f->normalized.pos.y,
+               f->normalized.vel.x,
+               f->normalized.vel.y,
+               f->identifier, f->state, f->foo3, f->foo4,
+               f->size, f->unk2);
+
+        const Color red(255, f->angle * 90 / atan2(1,0), 0);
+        canvas->SetPixel(f->normalized.pos.x * DISPLAY_WIDTH, (1.0f - f->normalized.pos.y) * DISPLAY_HEIGHT, red);
+        canvas->Send();
+    }
+    printf("\n");
+    return 0;
 }
 
 int main() {
-  MTDeviceRef dev = MTDeviceCreateDefault();
-  MTRegisterContactFrameCallback(dev, callback);
-  MTDeviceStart(dev, 0);
-  printf("Ctrl-C to abort\n");
-  sleep(-1);
-  return 0;
+    const int socket = OpenFlaschenTaschenSocket("10.20.4.57:1337");
+    canvas = new UDPFlaschenTaschen(socket, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+
+    MTDeviceRef dev = MTDeviceCreateDefault();
+    MTRegisterContactFrameCallback(dev, callback);
+    MTDeviceStart(dev, 0);
+    printf("Ctrl-C to abort\n");
+    sleep(-1);
+    return 0;
 }
