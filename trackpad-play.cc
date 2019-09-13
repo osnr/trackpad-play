441b122484091c64b3623d52d38d15a2c2301b9b
Omar Rizwan <omar.rizwan@gmail.com>
2019-09-12 18:11:27 -0700

    Fix screen clearing.

--- trackpad-play.cc
+++ trackpad-play.cc
@@ -49,25 +49,25 @@ void plotTouch(float x, float y) {
 }
 
 int callback(int device, Finger *data, int nFingers, double timestamp, int frame) {
-    memset(highres, 0, 500*500);
+    memset(highres, 0, 500*500*sizeof(int));
 
     canvas->Clear();
     for (int i=0; i<nFingers; i++) {
         Finger *f = &data[i];
     
-        printf("Frame %7d: Angle %6.2f, ellipse %6.3f x%6.3f; "
-               "position (%6.3f,%6.3f) vel (%6.3f,%6.3f) "
-               "ID %d, state %d [%d %d?] size %6.3f, %6.3f?\n",
-               f->frame,
-               f->angle * 90 / atan2(1,0),
-               f->majorAxis,
-               f->minorAxis,
-               f->normalized.pos.x,
-               f->normalized.pos.y,
-               f->normalized.vel.x,
-               f->normalized.vel.y,
-               f->identifier, f->state, f->foo3, f->foo4,
-               f->size, f->unk2);
+        // printf("Frame %7d: Angle %6.2f, ellipse %6.3f x%6.3f; "
+        //        "position (%6.3f,%6.3f) vel (%6.3f,%6.3f) "
+        //        "ID %d, state %d [%d %d?] size %6.3f, %6.3f?\n",
+        //        f->frame,
+        //        f->angle * 90 / atan2(1,0),
+        //        f->majorAxis,
+        //        f->minorAxis,
+        //        f->normalized.pos.x,
+        //        f->normalized.pos.y,
+        //        f->normalized.vel.x,
+        //        f->normalized.vel.y,
+        //        f->identifier, f->state, f->foo3, f->foo4,
+        //        f->size, f->unk2);
 
         plotTouch(f->normalized.pos.x, f->normalized.pos.y);
 
